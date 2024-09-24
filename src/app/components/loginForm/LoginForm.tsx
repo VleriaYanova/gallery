896e29eac191c51a365ac8b8ba0faffb1e441463
Valeria <v.yanova@rusbitech.ru>
2024-09-24 14:58:02 +0300

    add style for login form

--- src/app/components/loginForm/LoginForm.tsx
+++ src/app/components/loginForm/LoginForm.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import styles from './styles.module.scss'
 
 export default function LoginForm() {
-  return (
-    <div className={styles["login-container"]}>
-        <div className={styles["loign-inner"]}>
-            <h1>Log In</h1>
-            <input className={styles['login-input-form']} placeholder='enter your login' type="text"/>
-            <input className={styles['login-input-form']} placeholder='enter your password' type="text"/>
+    return (
+        <div className={styles["login-container"]}>
+            <div className={styles["loign-inner"]}>
+                <h1>Log In</h1>
+                <div className={styles['login-input']}>
+                    <input className={styles['login-input-form']} placeholder='enter your login' type="text" />
+                </div>
+                <div className={styles['password-input']}>
+                    <input className={styles['login-input-form']} placeholder='enter your password' type="text" />
+                </div>
+                <button className={styles['login-btn']}>ENTER</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
